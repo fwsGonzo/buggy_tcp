807c0c3f8a5920aa8f862837bf189087cc6ddfb1
Alf-André Walla <fwsgonzo@hotmail.com>
2019-05-06 11:43:29 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 mkdir -p build
 pushd build
-CC=clang-6.0
-CXX=clang++-6.0
 conan install .. includeos/latest -pr clang-6.0-linux-x86_64
+source activate.sh
 cmake ..
 make -j
-popd
+#popd
